67f91361178a09f050820ddc02ecfd094fab8eb7
luis nunez <l.nunez@blackstone.studio>
2026-03-31 15:32:58 -0600

    estructura del proyecto

---
+++ src/ReactGPT.tsx
@@ -0,0 +1,7 @@
+export const ReactGPT = () => {
+    return (
+        <div>
+            <h1>ReactGPT</h1>
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { ReactGPT } from './ReactGPT'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ReactGPT />
   </StrictMode>,
 )
