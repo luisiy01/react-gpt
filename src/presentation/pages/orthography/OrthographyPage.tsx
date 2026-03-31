b2a97c8bf91b32be1913475441bf09415633d3ed
luis nunez <l.nunez@blackstone.studio>
2026-03-31 17:16:56 -0600

    agregar burbujas de mensajes

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { GptMessage } from "../../components"
+import { MyMessage } from "../../components/chat-bubbles/MyMessage"
+
 
 export const OrthographyPage = () => {
     return (
-        <div>OrthographyPage</div>
+        <div className='chat-container'>
+            <div className='chat-messages'>
+                <div className='grid grid-cols-12 gap-y-2'>
+
+                    {/* Bienvenida */}
+                    <GptMessage text="Hola, puedes escribit tu texto" />
+                    <MyMessage text="Hola Mundo" />
+
+                </div>
+            </div>
+        </div>
     )
 }
