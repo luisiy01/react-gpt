27efa50f9dd8b7e048f2afa52772163d9105d887
luis nunez <l.nunez@blackstone.studio>
2026-04-01 12:26:35 -0600

    diseño audio to text page

--- src/presentation/pages/audio-to-text/AudioToTextPage.tsx
+++ src/presentation/pages/audio-to-text/AudioToTextPage.tsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import { useState } from "react"
+import { GptMessage, MyMessage, TextMessageBoxFile, TypingLoader } from "../../components"
+
+interface Message {
+    text: string;
+    isGpt: boolean;
+}
 
 export const AudioToTextPage = () => {
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [messages, setMessages] = useState<Message[]>([]);
+
+    const handlePost = async (text: string) => {
+        setIsLoading(true);
+        setMessages(prev => [...prev, { text, isGpt: false }]);
+
+        // TODO: 
+
+        setIsLoading(false);
+    }
+
     return (
-        <div>AudioToTextPage</div>
+        <div className='chat-container'>
+            <div className='chat-messages'>
+                <div className='grid grid-cols-12 gap-y-2'>
+
+                    {/* Bienvenida */}
+                    <GptMessage text="¿Que texto quieres generar hoy?" />
+
+                    {messages.map((message, index) => (
+                        message.isGpt ? (
+                            <GptMessage key={index} text={message.text} />
+                        ) : (
+                            <MyMessage key={index} text={message.text} />
+                        )
+                    ))}
+
+                    {
+                        isLoading && (
+                            <div className="col-start-1 col-end-12 fade-in">
+                                <TypingLoader />
+                            </div>
+                        )
+                    }
+
+
+                </div>
+            </div>
+
+            <TextMessageBoxFile
+                onSendMessage={handlePost}
+                placeholder="Escribe aqui tu pregunta"
+                disableCorrections
+            />
+        </div>
     )
 }
