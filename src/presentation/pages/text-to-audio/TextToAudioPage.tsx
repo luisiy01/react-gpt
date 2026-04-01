4af4a41be926c7fda685a784a0bc7a7ac66f6a66
luis nunez <l.nunez@blackstone.studio>
2026-04-01 10:57:15 -0600

    pantalla texto a audio

--- src/presentation/pages/text-to-audio/TextToAudioPage.tsx
+++ src/presentation/pages/text-to-audio/TextToAudioPage.tsx
@@ -1,7 +1,70 @@
-import React from 'react'
+import { useState } from "react"
+import { GptMessage, MyMessage, TextMessageBox, TextMessageBoxSelect, TypingLoader } from "../../components"
+
+const disclaimer = `## ¿Que audio quieres generar hoy?
+* Todo el audio generado es por IA`
+
+const voices = [
+    { id: "nova", text: "Nova" },
+    { id: "alloy", text: "Alloy" },
+    { id: "echo", text: "Echo" },
+    { id: "fable", text: "Fable" },
+    { id: "onyx", text: "Onyx" },
+    { id: "shimmer", text: "Shimmer" },
+]
+
+interface Message {
+    text: string;
+    isGpt: boolean;
+}
 
 export const TextToAudioPage = () => {
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [messages, setMessages] = useState<Message[]>([]);
+
+    const handlePost = async (text: string, selectedVoice: string) => {
+        setIsLoading(true);
+        setMessages(prev => [...prev, { text, isGpt: false }]);
+
+        // TODO: 
+
+        setIsLoading(false);
+    }
+
     return (
-        <div>TextToAudioPage</div>
+        <div className='chat-container'>
+            <div className='chat-messages'>
+                <div className='grid grid-cols-12 gap-y-2'>
+
+                    {/* Bienvenida */}
+                    <GptMessage text={disclaimer} />
+
+                    {messages.map((message, index) => (
+                        message.isGpt ? (
+                            <GptMessage key={index} text={message.text} />
+                        ) : (
+                            <MyMessage key={index} text={message.text} />
+                        )
+                    ))}
+
+                    {
+                        isLoading && (
+                            <div className="col-start-1 col-end-12 fade-in">
+                                <TypingLoader />
+                            </div>
+                        )
+                    }
+
+
+                </div>
+            </div>
+
+            <TextMessageBoxSelect
+                onSendMessage={handlePost}
+                placeholder="Escribe aqui tu pregunta"
+                options={voices}
+            />
+        </div>
     )
 }
