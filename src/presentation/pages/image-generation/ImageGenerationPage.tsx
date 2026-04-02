1f28936f67cb612b88b532e147e995c0e64a0efa
luis nunez <l.nunez@blackstone.studio>
2026-04-02 06:40:10 -0600

    actualizar image generation page

--- src/presentation/pages/image-generation/ImageGenerationPage.tsx
+++ src/presentation/pages/image-generation/ImageGenerationPage.tsx
@@ -1,7 +1,62 @@
-import React from 'react'
+import { useState } from "react"
+import { GptMessage, MyMessage, TextMessageBox, TypingLoader } from "../../components"
+
+interface Message {
+    text: string;
+    isGpt: boolean;
+    info?: {
+        imageUrl: string;
+        alt: string;
+    }
+}
 
 export const ImageGenerationPage = () => {
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [messages, setMessages] = useState<Message[]>([]);
+
+    const handlePost = async (text: string) => {
+        setIsLoading(true);
+        setMessages(prev => [...prev, { text, isGpt: false }]);
+
+        // TODO: 
+
+        setIsLoading(false);
+    }
+
     return (
-        <div>ImageGenerationPage</div>
+        <div className='chat-container'>
+            <div className='chat-messages'>
+                <div className='grid grid-cols-12 gap-y-2'>
+
+                    {/* Bienvenida */}
+                    <GptMessage text="¿Que imagen deseas generar hoy?" />
+
+                    {messages.map((message, index) => (
+                        message.isGpt ? (
+                            <GptMessage key={index} text={message.text} />
+                        ) : (
+                            <MyMessage key={index} text={message.text} />
+                        )
+                    ))}
+
+                    {
+                        isLoading && (
+                            <div className="col-start-1 col-end-12 fade-in">
+                                <TypingLoader />
+                            </div>
+                        )
+                    }
+
+
+                </div>
+            </div>
+
+            <TextMessageBox
+                onSendMessage={handlePost}
+                placeholder="Escribe aqui tu pregunta"
+                disableCorrections
+            />
+        </div>
     )
 }
