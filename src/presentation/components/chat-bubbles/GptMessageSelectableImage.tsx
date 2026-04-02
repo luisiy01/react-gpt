dc347a9108d166e04e55912679a1e94e89dd3efb
luis nunez <l.nunez@blackstone.studio>
2026-04-02 07:48:41 -0600

    convertir imagen en canvas

--- src/presentation/components/chat-bubbles/GptMessageSelectableImage.tsx
+++ src/presentation/components/chat-bubbles/GptMessageSelectableImage.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from "react";
+
 interface Props {
   imageUrl: string;
   alt: string;
@@ -7,6 +9,27 @@ interface Props {
 }
 
 export const GptMessageSelectableImage = ({ imageUrl }: Props) => {
+  const originalImageRef = useRef<HTMLImageElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const context = canvas.getContext("2d");
+    if (!context) return;
+
+    const image = new Image();
+    image.crossOrigin = "Anonymous";
+    image.src = imageUrl;
+
+    originalImageRef.current = image;
+
+    image.onload = () => {
+      context.drawImage(image, 0, 0, canvas.width, canvas.height);
+    };
+  }, []);
+
   return (
     <div className="col-start-1 col-end-9 p-3 rounded-lg">
       <div className="flex flex-row items-start">
@@ -14,7 +37,7 @@ export const GptMessageSelectableImage = ({ imageUrl }: Props) => {
           G
         </div>
         <div className="relative ml-3 text-sm bg-black bg-opacity-25 pt-3 pb-2 px-4 shadow rounded-xl">
-          <p>hola mundo</p>
+          <canvas ref={canvasRef} width={1024} height={1024} />
           {/* <img
             src={imageUrl}
             alt={alt}
