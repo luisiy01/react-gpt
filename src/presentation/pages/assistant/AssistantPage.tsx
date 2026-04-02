0689d56a31689479e9a15207f112442ac4a1db3d
luis nunez <l.nunez@blackstone.studio>
2026-04-02 15:12:53 -0600

    refactorizar archivos

--- src/presentation/pages/assistant/AssistantPage.tsx
+++ src/presentation/pages/assistant/AssistantPage.tsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import { useState } from "react";
+import {
+  GptMessage,
+  MyMessage,
+  TextMessageBox,
+  TypingLoader,
+} from "../../components";
+
+interface Message {
+  text: string;
+  isGpt: boolean;
+}
 
 export const AssistantPage = () => {
-    return (
-        <div>AssistantPage</div>
-    )
-}
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async (text: string) => {
+    setIsLoading(true);
+    setMessages((prev) => [...prev, { text, isGpt: false }]);
+
+    // TODO:
+
+    setIsLoading(false);
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          {/* Bienvenida */}
+          <GptMessage text="Buen dia, soy Sam, ¿En que puedo ayudarte?" />
+
+          {messages.map((message, index) =>
+            message.isGpt ? (
+              <GptMessage key={index} text={message.text} />
+            ) : (
+              <MyMessage key={index} text={message.text} />
+            ),
+          )}
+
+          {isLoading && (
+            <div className="col-start-1 col-end-12 fade-in">
+              <TypingLoader />
+            </div>
+          )}
+        </div>
+      </div>
+
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder="Escribe aqui tu pregunta"
+        disableCorrections
+      />
+    </div>
+  );
+};
